e0880df3e237c46e23586b2803775b0466feb9a6
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-09-07 06:54:41 +0200

    Snippet: Express server initialisation

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-const http = require('http');
-const fs = require('fs')
-const homePage = fs.readFileSync('./pages/home.html')
-const aboutPage = fs.readFileSync('./pages/about.html')
-const notFoundPage = fs.readFileSync('./pages/404.html') 
+const express = require('express')
+const app = express() // starts a new Express app
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/about") {
-    res.end(aboutPage)
-  } else if (req.url === "/") {
-    res.end(homePage)
-  } else {
-    res.writeHead(404)
-    res.end(notFoundPage)
-  }
+app.listen(3000, () => {
+	console.log('App listening on port 3000')
 })
-
-server.listen(3000)
